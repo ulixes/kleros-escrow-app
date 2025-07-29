effe9f4e0992f53fc886066c00bdbbb15a5d574e
Ulyxes <easel@ulyxes.xyz>
2025-07-29 16:22:00 +0000

    Updates

--- assets/all-wallets-v6m95Aar.js
+++
@@ -1,6 +0,0 @@
-import{m as a}from"./core-CzQ-8Id8.js";import"./index-DcRs-czL.js";import"./index.es-DJ6r7MyY.js";const i=a`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};
